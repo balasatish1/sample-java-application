9cc9b02900a4e2d29855bdad20fe7e0acc58dd0d
Bala Satish Malladi <balasatishmalladi@gmail.com>
2025-10-15 22:40:00 +0530

    complete backend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+import '../styles/Header.css';
+
+export default function Header() {
+
+  return(
+    <>
+      <div className="header">
+        <h1>Ride Sharing System</h1>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 
+import Header from '../components/Header.jsx';
 import '../styles/LandingPage.css';
 // import { useNavigate } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ export default function LandingPage() {
   // const navigate = useNavigate();
   return (
     <>
+      <Header/>
       <div className='overview-container'>
         <div className='content'>
           <h1>Uniting travelers through simple, shared journeys.
