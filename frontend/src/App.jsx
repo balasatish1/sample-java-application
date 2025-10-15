c8818fa63b32641226dc81a9202796ec23d95421
Bala Satish Malladi <balasatishmalladi@gmail.com>
2025-10-16 00:14:23 +0530

    set up the user registration form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage.jsx'
 import AdminLoginPage from './pages/AdminLoginPage.jsx'
 import AdminDashboard from './pages/AdminDashboard.jsx'
+import UserRegistrationPage from './pages/UserRegistrationPage.jsx'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/admin-login' element={<AdminLoginPage/>}/>
           <Route path='/admin-dashboard' element={<AdminDashboard/>}/>
+          <Route path='/user-registration' element={<UserRegistrationPage/>}/>
 
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/UserRegistrationPage.jsx
+++ frontend/src/pages/UserRegistrationPage.jsx
@@ -1,11 +1,56 @@
 
+import Header from '../components/Header.jsx';
 import '../styles/UserRegistrationPage.css';
 
 export default function UserRegistrationPage() {
 
   return (
+
     <>
-      <h1>user registration page</h1>
+      <Header />
+      <div className='user-registration-container'>
+        <h1>User Registration</h1>
+        <form className='user-registration-form'>
+          <label >Name</label>
+          <input
+            type="text"
+          />
+
+          <label>Email</label>
+          <input
+            type="email"
+          />
+
+
+          <label>Mobile</label>
+          <input
+            type="number"
+          />
+
+         <div className='user-type-input-box'>
+           <label>User type :</label>
+          <input type="radio" name='user-type' value="driver" /><label>Driver</label>
+          <input type="radio" name='user-type' value="passanger" /><label>Passanger</label>
+
+         </div>
+
+          <label >Username</label>
+          <input
+            type="text"
+          />
+
+
+          <label >Username</label>
+          <input
+            type="text"
+          />
+
+          <label>Password</label>
+          <input
+            type="password"
+          />
+        </form>
+      </div>
     </>
   );
 }
