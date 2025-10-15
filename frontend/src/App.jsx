03b9b85123257db98b8fbea03f67d367037d26c4
Bala Satish Malladi <balasatishmalladi@gmail.com>
2025-10-15 22:33:06 +0530

    setup the basic layout of the landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
     <>
-      <h1>Java Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,28 @@
+
+import '../styles/LandingPage.css';
+// import { useNavigate } from 'react-router-dom';
+
+
+export default function LandingPage() {
+  // const navigate = useNavigate();
+  return (
+    <>
+      <div className='overview-container'>
+        <div className='content'>
+          <h1>Uniting travelers through simple, shared journeys.
+            Your journey begins with just one connection</h1>
+          <button
+            className='get-started'
+          // onClick={() => navigate('/home')}
+          >
+            Get started
+          </button>
+        </div>
+
+        <div className='cover-image'>
+
+        </div>
+      </div>
+    </>
+  );
+}
